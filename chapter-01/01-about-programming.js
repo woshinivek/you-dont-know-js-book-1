67191c833481d1c7bc58166ce597dff58b54077d
woshinivek <woshinivek@yahoo.com>
2021-03-23 17:22:05 +0300

    changes

---
+++ chapter-01/01-about-programming.js
@@ -0,0 +1,36 @@
+// const pass = "admin";
+
+// const enterPass = prompt("Enter password plz");
+
+// const normalizedPass = enterPass.toLowerCase();
+
+// console.log(enterPass);
+// console.log(normalizedPass);
+// console.log(pass === normalizedPass);
+
+// const stars = 3;
+
+// switch (stars) {
+//   case 1:
+//     console.log("Отель 1 звезда");
+//     break;
+
+//   case 2:
+//     console.log("Отель 2 звезды");
+//     break;
+
+//   case 3:
+//     console.log("Отель 3 звезды");
+//     break;
+
+//   case 4:
+//     console.log("Отель 4 звезды");
+//     break;
+
+//   case 5:
+//     console.log("Отель 5 звезд");
+//     break;
+
+//   default:
+//     console.log("Непонимаю");
+// }
---
+++ chapter-01/02-about-js-1.js
@@ -0,0 +1,41 @@
+// let a;
+
+// console.log(typeof a);
+
+// a = "hello";
+// console.log("Hello: ", typeof a);
+
+// a = 42;
+// console.log("42: ", typeof a);
+
+// a = true;
+// console.log("true: ", typeof a);
+
+// a = false;
+// console.log("false: ", typeof a);
+
+// a = null;
+// console.log("null: ", typeof a);
+
+// a = [1, 2, 3];
+// console.log("[1,2,3]: ", typeof a);
+
+// a = { a: 2, d: 3 };
+// console.log("{a: 2, d: 3}; ", typeof a);
+
+// a = Symbol();
+// console.log("Symbol: ", typeof a);
+
+// a = undefined;
+// console.log("undefined: ", typeof a);
+
+const obj = {
+  a: "Hello",
+  b: 42,
+  c: true,
+};
+
+console.log(obj);
+
+const objA = "a";
+console.log(objA);
