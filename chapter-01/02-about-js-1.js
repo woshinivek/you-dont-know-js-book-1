fadab6c4ac3c19077f6270b5f670031587dae414
woshinivek <woshinivek@yahoo.com>
2021-03-27 16:02:47 +0300

    changes

--- chapter-01/02-about-js-1.js
+++ chapter-01/02-about-js-1.js
@@ -39,3 +39,31 @@ console.log(obj);
 
 const objA = "a";
 console.log(objA);
+
+// const objA = {
+//   a: 2,
+//   "hello hi": "Nihao brazer",
+// };
+
+// alert(objA["hello hi"]);
+
+// let key = "hello hi";
+
+// const objA = {
+//   a: 2,
+//   "hello hi": "Nihao brazer",
+// };
+
+// console.log(objA[key]);
+
+// console.log(key);
+
+let fruit = prompt("Введи фрукт");
+let quantity = Number(prompt("введи количество"));
+
+const bag = {
+  [fruit]: quantity,
+};
+
+console.log(`Вы добавили ${fruit} в количестве ${bag.quantity} штук`);
+console.log(bag.apple);
